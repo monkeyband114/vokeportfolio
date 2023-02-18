ecb837194b4c9a006b59338fa410885b8fb92c2d
Michael Voke <gadocer54@gmail.com>
2023-02-18 22:22:24 +0100

    Changes to be committed: 	modified:   src/components/Projects/Projects.jsx

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useLocation, useParams } from "react-router-dom";
-import Category from "./Category";
+
 import Project from "./Project";
 
 const Projects = () => {
